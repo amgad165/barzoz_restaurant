02e680b0ad13db145f6f2f7442e1ea539fb41ec4
Amgad <amgad.mourad18@gmail.com>
2023-12-25 05:39:57 +0200

    allowed hosts

--- barzoz_restaurant/settings.py
+++ barzoz_restaurant/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.21.145.40','127.0.0.1','bazroz.com']
 
 
 # Application definition
