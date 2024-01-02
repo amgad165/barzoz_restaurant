105f3f60cbcb19424fe33e036cff400a7216a44d
Amgad <amgad.mourad18@gmail.com>
2024-01-02 11:25:06 +0200

    being delivered added

--- base/admin.py
+++ base/admin.py
@@ -17,15 +17,33 @@ admin.site.register(Coupon, CouponAdmin)
 
 
 
+from django.contrib import admin
+from . models import *
+# Register your models here.
+admin.site.register(Menu_Item)
+
+admin.site.register(Category)
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'percent_off', 'duration')
+    # Exclude the stripe_coupon_id field from the admin page
+    exclude = ('stripe_coupon_id',)
+
+admin.site.register(Coupon, CouponAdmin)
+
+
+# admin.site.register(User_details)
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_summary', 'get_user_name', 'get_user_telefon', 'get_user_email',
-                    'get_bezirk', 'get_street_address', 'get_hausnummer', 'get_plz_zip', 'ordered_date',
+                    'get_bezirk', 'get_street_address', 'get_hausnummer', 'get_plz_zip', 'ordered_date', 'being_delivered',
                     'get_total')
-
+    list_editable = ('being_delivered',)  # Add this line to make 'being_delivered' editable
 
     def get_queryset(self, request):
         # Override get_queryset to include only orders where ordered = True
-        return super().get_queryset(request).filter(ordered=True)
+        return super().get_queryset(request).filter(ordered=True, being_delivered=False)
 
     def order_summary(self, obj):
         return ", ".join([f"{item.quantity} of {item.menu_item.name}" for item in obj.items.all()])
@@ -59,8 +77,6 @@ class OrderAdmin(admin.ModelAdmin):
         return obj.user_details.plz_zip if obj.user_details else ""
     get_plz_zip.short_description = 'PLZ/ZIP'
 
-
-
     def um_hinweise(self, obj):
         return obj.user_details.um_hinweise if obj.user_details else ""
     um_hinweise.short_description = 'Um Hinweise'
@@ -68,5 +84,9 @@ class OrderAdmin(admin.ModelAdmin):
     def get_total(self, obj):
         return obj.get_total()
     get_total.short_description = 'Total'
+
+    def being_delivered(self, obj):
+        return obj.being_delivered
+    being_delivered.short_description = 'Delivered'
 
 admin.site.register(Order, OrderAdmin)
