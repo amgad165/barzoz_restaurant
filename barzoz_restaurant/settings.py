3466cfc9c2f50d4244ad3bdba2384a1bb399b4c0
Amgad <amgad.mourad18@gmail.com>
2024-01-06 01:46:05 +0200

    .

--- barzoz_restaurant/settings.py
+++ barzoz_restaurant/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.21.145.40','127.0.0.1','bazroz.com']
 
